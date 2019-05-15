32066bfa681725e5e5da6349f5201547bafbb3a2
Andrés Rodríguez <30989783+arodr10@users.noreply.github.com>
2019-05-14 20:52:13 -0500

    Add files via upload

---
+++ form-insert/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-layout justify-center align-center>
+      <v-flex xs12 sm8 md6 lg5 xl4>
+        <v-card class="elevation-6">
+          <v-toolbar color="primary" dark card>
+            <v-toolbar-title>
+                Datos Básicos de Cliente Persona Natural
+            </v-toolbar-title>
+          </v-toolbar>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-app>
+</template>
